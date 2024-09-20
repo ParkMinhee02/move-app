07b8208993c1253d8fc8d6c58a8d65b47b143ef5
ParkMinhee02 <sundu32672@naver.com>
2024-09-20 19:43:40 +0900

    영화 데이터 불러오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Movies from "./routes/Movies";
+import Navbar from "./components/Navbar";
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	/* const onSubmit = () => {
+		alert('안녕')
+	}; */
+	return (
+	<div className="App">
+		{/* <input type="text" onKeyUp={(e) => {
+			if(e.keyCode === 13){onSubmit()}; //Enter키 입력 시 버튼 누르는 것과 같은 효과
+		}} />
+		<button onClick={onSubmit}>Submit</button> */}
+
+		<Navbar />
+
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/about:id' element={<About />} />
+			<Route path='/movies' element={<Movies />} />
+		</Routes>
+	</div>
+);
 }
 
 export default App;
