876581f626dcf720e55b8d6971b60393e111a367
ParkMinhee02 <sundu32672@naver.com>
2024-10-10 15:31:46 +0900

    Last

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,45 +5,58 @@ import Appmovie from '../components/Appmovie';
 import Upcoming from '../components/Upcoming';
 
 const Home = () => {
-	const [appMovie, setAppMovie] = useState([]);
-	const [isLoading, setLoading] = useState(true);
-	const [visibleMovies, setVisibleMovies] = useState(6);
-	const moviesPerPage = 3;
+	
+   const APIKEY=process.env.REACT_APP_API_KEY;
 
-	const getMovies = async () =>{
-		
-	}
+   const [appMovie, setAppMovie] = useState([]);
+   const [isLoading, setLoading]= useState(true);
+   const [visibleMovies, setVisibleMovies]=useState(6);
+   const moviesPerPage=3;
 
-	useEffect(() => {
-		getMovies();
-	},[]);
+   const getMovies = async () =>{
+      try{
+         const response= await axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${APIKEY}&language=ko-KR`);
+         setAppMovie(response.data.results)
+         /* console.log(response.data) */
+         setLoading(false)
 
-	const handleLodeMore = () => {
-		setVisibleMovies((prevVisible) => prevVisible+moviesPerPage);
-	};
+      }catch(error){
+         console.log('Error:',error )
+         setLoading(false)
+      }
+   }
 
-	return (
-		<>
-			<Upcoming />
-			<div className='homeWrap'>
-				<h2>상영작</h2>
-				{
-					isLoading ? (<div><span className='load'>Loading...</span></div>):(
-						<div className="appWrap">
-							{appMovie.slice(0, visibleMovies).map((amovie) => (<Appmovie key={amovie.id} id={amovie.id} title={amovie.title} poster_path={amovie.poster_path} release_date={amovie.release_date}/>))}
-						</div>
-					)
-				}
-				{
-					appMovie.length > visibleMovies && (
-						<div className='more'>
-							<button className='moreBtn' onClick={handleLodeMore}>More</button>
-						</div>
-					)
-				}
-			</div>
-		</>
-	);
+
+   useEffect(()=>{
+      getMovies();
+   }, []);
+
+   const handleLodeMore=()=>{
+      setVisibleMovies((prevVisible) =>prevVisible+moviesPerPage );
+   }
+   return (
+      <>
+         <Upcoming/>
+         <div className='homeWrap'>
+            <h2>상영작</h2>
+            {
+               isLoading ? (<div><span className='load'>Loading...</span></div>):(
+                  <div className="appWrap">
+                     {appMovie.slice(0, visibleMovies).map((amovie)=>(<Appmovie key={amovie.id} id={amovie.id} title={amovie.title} poster_path={amovie.poster_path} release_date={amovie.release_date}/>))}
+                  </div>
+               )
+            }
+            {
+               appMovie.length > visibleMovies && (
+                  <div className='more'>
+                     <button className='moreBtn' onClick={handleLodeMore}>더보기</button>
+                  </div>
+               )
+            }
+
+         </div>
+      </>
+   );
 };
 
 export default Home;
